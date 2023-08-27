b65facbb5acf7f3bd99b2ee579f62516a1a50f37
Kevin Song <song_zj@hotmail.com>
2023-08-26 20:19:15 -0500

    new f

--- js/csv_to_html_table.js
+++ js/csv_to_html_table.js
@@ -55,8 +55,8 @@ CsvToHtmlTable = {
                 if (allow_download) {
                     $containerElement.append("<p><a class='btn btn-info' href='" + csv_path + "'><i class='glyphicon glyphicon-download'></i> Download as CSV</a></p>");
                 }
-
-                if(document.getElementsByTagName('th')[1].innerHTML=='avg_rank'){
+                document.getElementsByTagName('th')[1].click();
+                if(document.getElementsByTagName('th')[1].innerHTML!='avg_rank'){
                     document.getElementsByTagName('th')[1].click();
                 }
             });
