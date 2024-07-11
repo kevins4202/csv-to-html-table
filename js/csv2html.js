9a2bb8b8cfe1f6faba3db779ecbffab4b9bc5d55
Mack Navickas <mjnavickas@gmail.com>
2024-07-11 16:07:59 -0500

    Reorganized csv2html for future new page

--- js/csv2html.js
+++ js/csv2html.js
@@ -2,8 +2,9 @@ import { parse } from './csvparse.js';
 
 function table(options) {
     options = options || {};
-    var csvPath = options.csvPath || "";
-    var csvElement = options.csvElement || "";
+    var csvPath = options.csvPath || '';
+    var csvElement = options.csvElement || '';
+    var processor = options.processor || ((element, val, colIdx, rowIdx) => element.textContent = val);
 
     var table = document.createElement('table');
     table.className = 'table table-striped table-condensed';
@@ -38,8 +39,7 @@ function table(options) {
             for (var rowIdx = 1; rowIdx < csvData.length; rowIdx++) {
                 var tableBodyRow = document.createElement('tr');
                 for (var colIdx = 0; colIdx < csvData[rowIdx].length; colIdx++) {
-                    var tableBodyRowTd = document.createElement('td');
-                    tableBodyRowTd.textContent = csvData[rowIdx][colIdx];
+                    var tableBodyRowTd = processor(document.createElement('td'), csvData[rowIdx][colIdx], rowIdx, colIdx);
                     tableBodyRow.appendChild(tableBodyRowTd);
                 }
                 tableBody.appendChild(tableBodyRow);
@@ -47,22 +47,15 @@ function table(options) {
 
             table.append(tableBody);
 
-            $(table).DataTable({ "paging": false });
+            $(table).DataTable({ 'paging': false });
 
             var downloadLink = document.createElement('a');
             downloadLink.className = 'btn btn-info';
             downloadLink.href = `csv/${metric}_ranking.csv`;
-            downloadLink.innerHTML = "<i class='glyphicon glyphicon-download'></i> Download this data as CSV";
+            downloadLink.innerHTML = '<i class="glyphicon glyphicon-download"></i> Download this data as CSV';
             containerElement.appendChild(downloadLink);
         })
-        .catch(error => console.error('Error fetching the CSV file:', error));
+        .catch(error => console.error('Parse error', error));
 }
 
-function bigTable(metric) {
-    table({
-        csvPath: `csv/${metric}_ranking.csv`,
-        csvElement: 'bigTable'
-    })
-}
-
-export { bigTable };
+export { table };
