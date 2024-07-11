6b302c4d1a7b112c98f69d2b8f5ce7afad9502af
Mack Navickas <mjnavickas@gmail.com>
2024-07-10 23:01:10 -0500

    Changed csv parser and simplified csv2html.js

--- js/csv2html.js
+++ js/csv2html.js
@@ -1,60 +1,60 @@
-var Csv2Html = Csv2Html || {};
+import { parse } from './csvparse.js';
 
-Csv2Html = {
-    table: function (options) {
-        options = options || {};
-        var csv_path = options.csv_path || "";
-        var el = options.element || "table-container";
-        var allow_download = options.allow_download || false;
-        var csv_options = options.csv_options || {};
-        var datatables_options = options.datatables_options || {};
-        var custom_formatting = options.custom_formatting || [];
-        var customTemplates = {};
-        $.each(custom_formatting, function (i, v) {
-            var colIdx = v[0];
-            var func = v[1];
-            customTemplates[colIdx] = func;
-        });
+function table(options) {
+    options = options || {};
+    var csv_path = options.csv_path || "";
 
-        var $table = $("<table class='table table-striped table-condensed' id='" + el + "-table'></table>");
-        var $containerElement = $("#" + el);
-        $containerElement.empty().append($table);
+    var table = document.createElement('table');
+    table.className = 'table table-striped table-condensed';
+    table.id = 'tableElement-table';
 
-        $.when($.get(csv_path)).then(
-            function (data) {
-                var csvData = $.csv.toArrays(data, csv_options);
-                var $tableHead = $("<thead></thead>");
-                var csvHeaderRow = csvData[0];
-                var $tableHeadRow = $("<tr></tr>");
-                for (var headerIdx = 0; headerIdx < csvHeaderRow.length; headerIdx++) {
-                    $tableHeadRow.append($("<th></th>").text(csvHeaderRow[headerIdx]));
-                }
-                $tableHead.append($tableHeadRow);
+    var containerElement = document.getElementById('tableElement');
 
-                $table.append($tableHead);
-                var $tableBody = $("<tbody></tbody>");
+    containerElement.innerHTML = '';
+    containerElement.appendChild(table);
 
-                for (var rowIdx = 1; rowIdx < csvData.length; rowIdx++) {
-                    var $tableBodyRow = $("<tr></tr>");
-                    for (var colIdx = 0; colIdx < csvData[rowIdx].length; colIdx++) {
-                        var $tableBodyRowTd = $("<td></td>");
-                        var cellTemplateFunc = customTemplates[colIdx];
-                        if (cellTemplateFunc) {
-                            $tableBodyRowTd.html(cellTemplateFunc(csvData[rowIdx][colIdx]));
-                        } else {
-                            $tableBodyRowTd.text(csvData[rowIdx][colIdx]);
-                        }
-                        $tableBodyRow.append($tableBodyRowTd);
-                        $tableBody.append($tableBodyRow);
-                    }
-                }
-                $table.append($tableBody);
+    fetch(csv_path)
+        .then(response => response.text())
+        .then(data => {
+            var csvData = parse(data);
 
-                $table.DataTable(datatables_options);
+            var tableHead = document.createElement('thead')
+            var csvHeaderRow = csvData[0];
+            var tableHeadRow = document.createElement('tr')
 
-                if (allow_download) {
-                    $containerElement.append("<p><a class='btn btn-info' href='" + csv_path + "'><i class='glyphicon glyphicon-download'></i> Download as CSV</a></p>");
-                }
+            csvHeaderRow.forEach(header => {
+                var th = document.createElement('th');
+                th.textContent = header;
+                tableHeadRow.appendChild(th);
             });
-    }
-};
+
+            tableHead.appendChild(tableHeadRow);
+
+            table.append(tableHead);
+
+            var tableBody = document.createElement('tbody');
+
+            for (var rowIdx = 1; rowIdx < csvData.length; rowIdx++) {
+                var tableBodyRow = document.createElement('tr');
+                for (var colIdx = 0; colIdx < csvData[rowIdx].length; colIdx++) {
+                    var tableBodyRowTd = document.createElement('td');
+                    tableBodyRowTd.textContent = csvData[rowIdx][colIdx];
+                    tableBodyRow.appendChild(tableBodyRowTd);
+                }
+                tableBody.appendChild(tableBodyRow);
+            }
+
+            table.append(tableBody);
+
+            $(table).DataTable({ "paging": false });
+
+            var downloadLink = document.createElement('a');
+            downloadLink.className = 'btn btn-info';
+            downloadLink.href = csv_path;
+            downloadLink.innerHTML = "<i class='glyphicon glyphicon-download'></i> Download this data as CSV";
+            containerElement.appendChild(downloadLink);
+        })
+        .catch(error => console.error('Error fetching the CSV file:', error));
+}
+
+export { table };
